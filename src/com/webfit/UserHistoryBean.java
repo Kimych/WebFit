aae52179bae8640ee8b7e8f10c658bd2600982f9
SergioAlecky <sergio.alecky@uniterra.by>
2015-02-10 12:26:51 +0300

    #1 new view userHistory table (formatted)

--- src/com/webfit/UserHistoryBean.java
+++ src/com/webfit/UserHistoryBean.java
@@ -1,5 +1,7 @@
 package com.webfit;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,27 +11,90 @@ import javax.faces.bean.SessionScoped;
 import by.uniterra.dai.eao.DaysOfWorkEAO;
 import by.uniterra.dai.entity.DaysOfWork;
 import by.uniterra.system.model.SystemModel;
+import by.uniterra.system.util.DateUtils;
+import by.uniterra.system.util.WorkLogUtils;
 
 @ManagedBean(name = "userHistoryBean")
 @SessionScoped
-public class UserHistoryBean
+public class UserHistoryBean implements Serializable
 {
-    
-    private List<DaysOfWork> lslDaysOfWorks = new ArrayList<DaysOfWork>();
 
-    public UserHistoryBean()
+    /** TODO document <code>serialVersionUID</code> */
+    private static final long serialVersionUID = 1L;
+
+    private List<UserHistoryData> lstUserHistory = new ArrayList<UserHistoryData>();
+
+    public List<UserHistoryData> getLstUserHistory()
     {
+        lstUserHistory.clear();
         DaysOfWorkEAO dofEAO = new DaysOfWorkEAO(SystemModel.getDefaultEM());
-        this.lslDaysOfWorks = dofEAO.finAllByWorker(SystemModel.getAuthorization().getWorker());
+        List<DaysOfWork> lslDaysOfWorks = dofEAO.finAllByWorker(SystemModel.getAuthorization().getWorker());
+        for (DaysOfWork daysOfWork : lslDaysOfWorks)
+        {
+            lstUserHistory.add(new UserHistoryData(daysOfWork));
+        }
+        return lstUserHistory;
     }
-    
-    public List<DaysOfWork> getLslDaysOfWorks()
+
+    public void setLstUserHistory(List<UserHistoryData> lslUSerHistory)
     {
-        return lslDaysOfWorks;
+        this.lstUserHistory = lslUSerHistory;
     }
-    public void setLslDaysOfWorks(List<DaysOfWork> lslDaysOfWorks)
+
+    public static class UserHistoryData
     {
-        this.lslDaysOfWorks = lslDaysOfWorks;
+        String timestamp;
+        String worklog;
+        String bonusTime;
+        int aktualWorkedDays;
+
+        public UserHistoryData(DaysOfWork objDoW)
+        {
+            this.timestamp = DateUtils.toGMT(objDoW.getTimestamp());
+            this.worklog = WorkLogUtils.roundToString(objDoW.getWorklog(), 2, BigDecimal.ROUND_HALF_UP);
+            this.bonusTime = WorkLogUtils.roundToString(objDoW.getBonusTime(), 2, BigDecimal.ROUND_HALF_UP);
+            this.aktualWorkedDays = objDoW.getAktualWorkedDays();
+        }
+
+        public String getTimestamp()
+        {
+            return timestamp;
+        }
+
+        public void setTimestamp(String timestamp)
+        {
+            this.timestamp = timestamp;
+        }
+
+        public String getWorklog()
+        {
+            return worklog;
+        }
+
+        public void setWorklog(String worklog)
+        {
+            this.worklog = worklog;
+        }
+
+        public String getBonusTime()
+        {
+            return bonusTime;
+        }
+
+        public void setBonusTime(String bonusTime)
+        {
+            this.bonusTime = bonusTime;
+        }
+
+        public int getAktualWorkedDays()
+        {
+            return aktualWorkedDays;
+        }
+
+        public void setAktualWorkedDays(int aktualWorkedDays)
+        {
+            this.aktualWorkedDays = aktualWorkedDays;
+        }
     }
-    
+
 }
